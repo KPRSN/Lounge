6907519b8197c1ba038f0de792d73c04319ed29b
GaffelGaffel <k.julmust@gmail.com>
2014-08-31 14:03:39 +0200

    Initial Commit

---
+++ Spotibar/SBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SBAppDelegate.h
+//  Spotibar
+//
+//  Created by Karl Persson on 2014-08-31.
+//  Copyright (c) 2014 Karl Persson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SBAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Spotibar/SBAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  SBAppDelegate.m
+//  Spotibar
+//
+//  Created by Karl Persson on 2014-08-31.
+//  Copyright (c) 2014 Karl Persson. All rights reserved.
+//
+
+#import "SBAppDelegate.h"
+
+@implementation SBAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
